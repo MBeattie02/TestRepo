e4bb1a39ad71c58a776d7e7f3946f59a7d3a4c89
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-10-31 09:52:09 +0000

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Simple Calculator");
+        System.out.println("-----------------");
+
+        while (true) {
+            System.out.print("Enter first number (or type 'exit' to quit): ");
+            String input1 = scanner.next();
+
+            if (input1.equalsIgnoreCase("exit")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            System.out.print("Enter operator (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+
+            System.out.print("Enter second number: ");
+            double number2 = scanner.nextDouble();
+
+            try {
+                double number1 = Double.parseDouble(input1);
+                double result = calculate(number1, operator, number2);
+                System.out.println("Result: " + result);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number entered. Please try again.");
+            } catch (ArithmeticException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static double calculate(double number1, char operator, double number2) {
+        switch (operator) {
+            case '+':
+                return number1 + number2;
+            case '-':
+                return number1 - number2;
+            case '*':
+                return number1 * number2;
+            case '/':
+                if (number2 == 0) {
+                    throw new ArithmeticException("Cannot divide by zero.");
+                }
+                return number1 / number2;
+            default:
+                throw new IllegalArgumentException("Invalid operator. Supported operators are +, -, *, /.");
+        }
+    }
+}
