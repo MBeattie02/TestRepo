ec10dd4acb9d0c609c0d4fffae80a4178baddc53
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-11-02 18:56:42 +0000

    Update SQL.java

--- SQL.java
+++ SQL.java
@@ -1,6 +1,20 @@
-public void queryDatabase(String tableName) {
-    String query = "SELECT * FROM " + tableName;
-    Statement stmt = connection.createStatement();
-    ResultSet rs = stmt.executeQuery("SELECT * FROM Users WHERE username='admin'");  // This line would be flagged
-    // ...
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class VulnerableClass {
+    
+    private Connection connection;
+
+    public void queryDatabase(String tableName, String columnName, String value) {
+        try {
+            String query = "SELECT * FROM " + tableName + " WHERE " + columnName + "='" + value + "'";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            // ...
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
