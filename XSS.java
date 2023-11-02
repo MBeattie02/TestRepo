fcaf1a6b9fff9db21e13b8315026099fe6b50790
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-11-02 19:39:58 +0000

    Update XSS.java

--- XSS.java
+++ XSS.java
@@ -1,25 +1,18 @@
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-
-@WebServlet("/greet")
-public class GreetingServlet extends HttpServlet {
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-        // User input is retrieved from the request
-        String username = request.getParameter("username");
-
-        // User input is directly used in the HTML response without sanitization
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<h1>Hello, " + username + "!</h1>"); // Potential XSS vulnerability
-        out.println("</body>");
-        out.println("</html>");
+public class SimpleWebApp {
+    public static void main(String[] args) {
+        String userInput = getParameter("userInput"); // Simulate getting a parameter from a web request
+        printToWebPage("Welcome, " + userInput); // This line is vulnerable to XSS
     }
 
-    // ... other methods ...
+    // Simulated method to mimic getting user input from a web request
+    public static String getParameter(String paramName) {
+        // In a real scenario, this would return user-supplied input
+        return "userSuppliedContent";
+    }
+
+    // Simulated method to mimic sending output to a web page
+    public static void printToWebPage(String content) {
+        // In a real scenario, this would output directly to the web page
+        System.out.println(content);
+    }
 }
