8567df1a784034f67a12e23ac1ec039d727d641e
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-11-02 18:46:02 +0000

    Update SQL.java

--- SQL.java
+++ SQL.java
@@ -1,26 +1,6 @@
-public class UserService {
-
-    // Simulated method that returns user-controlled input
-    public String getUserInput() {
-        // In a real application, this could be input from a web form, URL parameter, etc.
-        return "'; DROP TABLE users; --";
-    }
-
-    // Method that constructs and executes a SQL query including user input directly
-    public void deleteUser(String username) {
-        String userInput = getUserInput();
-        DatabaseConnection dbConnection = new DatabaseConnection();
-        
-        // The following line is vulnerable to SQL Injection
-        String sql = "DELETE FROM users WHERE username = '" + userInput + "'";
-        
-        dbConnection.executeQuery(sql);
-    }
-}
-
-class DatabaseConnection {
-    public void executeQuery(String sql) {
-        // Executes the SQL query on the database
-        // ...
-    }
+public void queryDatabase(String tableName) {
+    String query = "SELECT * FROM " + tableName;
+    Statement stmt = connection.createStatement();
+    ResultSet rs = stmt.executeQuery("SELECT * FROM Users WHERE username='admin'");  // This line would be flagged
+    // ...
 }
