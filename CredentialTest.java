6cb398f16247017e0a569ba198da9131231e46bf
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-11-03 13:29:02 +0000

    Update and rename XSS.java to CredentialTest.java

---
+++ CredentialTest.java
@@ -0,0 +1,20 @@
+public class CredentialTest {
+    
+    private String userPassword = "password123";
+    
+    public void connectToDatabase() {
+        String connectionToken = "secretToken";
+        String dbConnection = "jdbc:database://localhost:3306/users?user=admin&password=" + userPassword;
+        // ...
+    }
+    
+    public void someMethod() {
+        sendRequest("https://api.example.com/login", "apiKey=secretApiKey");
+        // ...
+    }
+    
+    private void sendRequest(String url, String headers) {
+        // ...
+    }
+}
+
