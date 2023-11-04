2e7827c42a70b1b61b6f8ce1cb28800ed1534a7c
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-11-04 11:45:41 +0000

    Create Duplicate.java

---
+++ Duplicate.java
@@ -0,0 +1,18 @@
+public class Duplicate {
+
+    public void method1() {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+    public void method2() {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
