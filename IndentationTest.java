09128d12a0ea863a46f7949661c6d82339eaf909
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-11-04 10:54:18 +0000

    Update IndentationTest.java

--- IndentationTest.java
+++ IndentationTest.java
@@ -1,42 +1,27 @@
 public class IndentationTest {
 
     public static void main(String[] args) {
-        // Correct indentation for a method block
-        int n = 10;
-        for (int i = 0; i < n; i++) {
-            // Correct indentation for a loop block
-            if (i % 2 == 0) {
-                // Correct indentation for an if block
-                System.out.println(i + " is even");
-            } else {
-                // Incorrect indentation, should be 4 spaces from the 'if' block
-              System.out.println(i + " is odd");
-            }
-        }
+        System.out.println("Test 1");
+            System.out.println("Indentation error here"); // Incorrect: Should be 8 spaces
+    }
 
-        // Another method with incorrect indentation at the start
-      List<String> list = new ArrayList<>();
-        // Correct indentation for the method block
-        for (String item : list) {
-            // Correct indentation for a for-each loop
-            System.out.println(item);
-        }
-
-        // Nested loops with correct indentation
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j < i) {
-                    // Incorrect indentation, should be 4 spaces from the 'if' block
-                  System.out.println("j < i");
+    public void methodExample() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("In for loop");
+                if(i % 2 == 0) {
+                    System.out.println("Number is even"); // Correct: 16 spaces (8 + 8)
+                } else {
+                            System.out.println("Number is odd"); // Incorrect: Should be 16 spaces
                 }
-            }
         }
     }
 
-    // Incorrect indentation for a method declaration, should be at the class level
-  public void incorrectlyIndentedMethod() {
-        // Correct indentation for the method block
-        System.out.println("This method is incorrectly indented.");
+    public void anotherMethod() {
+          while(true) {
+            System.out.println("In while loop"); // Incorrect: Should be 8 spaces
+            break;
+          }
     }
 }
+
 
