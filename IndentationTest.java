7bffdf3e18ab5c21c229618b775e1949f3fb3d80
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2023-11-04 11:01:09 +0000

    Update IndentationTest.java

--- IndentationTest.java
+++ IndentationTest.java
@@ -1,26 +1,17 @@
 public class IndentationTest {
 
     public static void main(String[] args) {
-        System.out.println("Test 1");
-            System.out.println("Indentation error here"); // Incorrect: Should be 8 spaces
-    }
-
-    public void methodExample() {
-        for(int i = 0; i < 10; i++) {
-            System.out.println("In for loop");
-                if(i % 2 == 0) {
-                    System.out.println("Number is even"); // Correct: 16 spaces (8 + 8)
-                } else {
-                            System.out.println("Number is odd"); // Incorrect: Should be 16 spaces
-                }
+        System.out.println("Hello, World!"); // Correctly indented
+       System.out.println("This line is indented incorrectly."); // Incorrectly indented
+        if (args.length > 0) {
+            System.out.println("Arguments provided"); // Correctly indented
+          System.out.println("This line is also indented incorrectly."); // Incorrectly indented
         }
     }
 
     public void anotherMethod() {
-          while(true) {
-            System.out.println("In while loop"); // Incorrect: Should be 8 spaces
-            break;
-          }
+      System.out.println("Incorrect indentation here."); // Incorrectly indented
+        System.out.println("Correct indentation."); // Correctly indented
     }
 }
 
